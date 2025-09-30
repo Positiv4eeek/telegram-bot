0545a679ab29f1f97dd332407679500d8255cb7a
Miras <131511790+Positiv4eeek@users.noreply.github.com>
2025-09-30 16:01:45 +0500

    fix ipv4

--- app/bot.py
+++ app/bot.py
@@ -1,13 +1,19 @@
 import socket
 from aiohttp import TCPConnector
 from aiogram import Bot, Dispatcher
+from aiogram.client.session.aiohttp import AiohttpSession
+
 from app.core.config import settings
 from app.core.telemetry import UserMiddleware
 
-connector = TCPConnector(family=socket.AF_INET)
+def create_bot_dp() -> tuple[Bot, Dispatcher]:
+    connector = TCPConnector(family=socket.AF_INET)
+    session = AiohttpSession(connector=connector)
 
-bot = Bot(token=settings.bot_token, connector=connector)
-dp = Dispatcher()
+    bot = Bot(token=settings.bot_token, session=session)
+    dp = Dispatcher()
 
-dp.message.middleware(UserMiddleware())
-dp.callback_query.middleware(UserMiddleware())
+    dp.message.middleware(UserMiddleware())
+    dp.callback_query.middleware(UserMiddleware())
+
+    return bot, dp
