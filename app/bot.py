151fe6b5b7c5001ebfc4e202368f58cbaf852161
Miras <131511790+Positiv4eeek@users.noreply.github.com>
2025-09-30 15:55:02 +0500

    Update bot.py

--- app/bot.py
+++ app/bot.py
@@ -1,8 +1,12 @@
+import socket
+from aiohttp import TCPConnector
 from aiogram import Bot, Dispatcher
 from app.core.config import settings
 from app.core.telemetry import UserMiddleware
 
-bot = Bot(token=settings.bot_token)
+connector = TCPConnector(family=socket.AF_INET)
+
+bot = Bot(token=settings.bot_token, connector=connector)
 dp = Dispatcher()
 
 dp.message.middleware(UserMiddleware())
