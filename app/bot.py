d31feb283ac50ce13d7c092b2c2a140a65b46147
Miras <131511790+Positiv4eeek@users.noreply.github.com>
2025-09-30 16:10:36 +0500

    fix

--- app/bot.py
+++ app/bot.py
@@ -1,19 +1,13 @@
 import socket
 from aiohttp import TCPConnector
 from aiogram import Bot, Dispatcher
-from aiogram.client.session.aiohttp import AiohttpSession
-
 from app.core.config import settings
 from app.core.telemetry import UserMiddleware
 
-def create_bot_dp() -> tuple[Bot, Dispatcher]:
-    connector = TCPConnector(family=socket.AF_INET)
-    session = AiohttpSession(connector=connector)
+connector = TCPConnector(family=socket.AF_INET)
 
-    bot = Bot(token=settings.bot_token, session=session)
-    dp = Dispatcher()
+bot = Bot(token=settings.bot_token, connector=connector)
+dp = Dispatcher()
 
-    dp.message.middleware(UserMiddleware())
-    dp.callback_query.middleware(UserMiddleware())
-
-    return bot, dp
+dp.message.middleware(UserMiddleware())
+dp.callback_query.middleware(UserMiddleware())
